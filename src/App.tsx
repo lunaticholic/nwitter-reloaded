c4c352cd0257334f903b7fc776f9851acf95690b
lunaticholic <lunaticholic@gmail.com>
2024-11-02 11:48:05 +0100

    Nwitter Forms and UI

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Home from "./routes/home";
 import Profile from "./routes/profile";
 import Login from "./routes/login";
 import CreateAccount from "./routes/create-account";
-import { createGlobalStyle } from "styled-components";
+import styled, { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
 import { useEffect, useState } from "react";
 import LoadingScreen from "./components/loading-screen";
@@ -47,9 +47,16 @@ const GlobalStyle = createGlobalStyle`
   }
 `;
 
+const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+`;
+
 function App() {
   const [isLoading, setLoading] = useState(true);
   const init = async() => {
+    //firebase가 쿠키와 토근을 읽고 백엔드와 소통해서 로그인여부를 확인하는 동안 기다리도록 한다
     await auth.authStateReady();
 
     // wait for firebase
@@ -61,12 +68,12 @@ function App() {
   }, []);
 
   return (
-    <>
-      // 아래의 스타일은 현재 페이지에 작성된 css스타일을 따라가도록 한다.
+    <Wrapper>
+      {/* 아래의 스타일은 현재 페이지에 작성된 css스타일을 따라가도록 한다. */}
       <GlobalStyle />
-      // 로딩이 진행되는 경우에은 LoadingScreen을 보여주고 아니면 router를 통해 해당 페이지를 보여준다
+      {/* 로딩이 진행되는 경우에은 LoadingScreen을 보여주고 아니면 router를 통해 해당 페이지를 보여준다 */}
       { isLoading ? <LoadingScreen /> : <RouterProvider router={router} />}
-    </>
+    </Wrapper>
   );
 }
 
--- src/routes/create-account.tsx
+++ src/routes/create-account.tsx
@@ -1,3 +1,93 @@
+import { useState } from "react";
+import { styled } from "styled-components";
+
+const Wrapper = styled.div`
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 420px;
+    padding: 50px 0px;
+`;
+const Title = styled.h1`
+    font-size: 42px
+`;
+const Form = styled.form`
+    margin-top: 50px;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    width: 100%
+`;
+const Input = styled.input`
+    padding: 10px 20px;
+    border-radius: 50px;
+    border: none;
+    width: 100%;
+    font-size: 16px;
+    &[type="submit"] {
+        cursor: pointer;
+        &:hover {
+            opacity: 0.8;
+        }
+    }
+`;
+const Error = styled.span`
+    font-weight: 600;
+    color: tomato;
+`;
+
 export default function CreateAccount(){
-    return <h1>create account</h1>
+    const [isLoading, setLoading] = useState(false);
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { target: {name, value} } = e;
+        // name에 값이 입력되면 setName에 저장
+        if (name === "name") {
+            setName(value)
+        
+        }
+        // email에 값이 입력되면 setEmail에 저장
+        else if (name === "email"){
+            setEmail(value)
+        } 
+        // password에 값이 입력되면 setPassword에 저장
+        else if (name === "password") {
+            setPassword(value)
+        }
+    };
+    const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        //form에 값이 다 입력되고 submit 버튼을 클릭하면 페이지를 새로그침
+        e.preventDefault();
+        try {
+            // 계정 생성하기
+
+            // user의 이름을 정의
+
+            // 계정 생성이 완료되면 홈페이지로 페이지 이동
+        } catch(e) {
+            // 에러가 발생할 경우 무슨 에러인지 확인
+
+        } finally {
+            setLoading(false)
+        }
+        
+        console.log(name, email, password);
+    }
+
+    return <Wrapper>
+        <Title>Login in 🅧</Title>
+        <Form onSubmit={onSubmit}>
+            <Input onChange={onChange} name="name" value={name} placeholder="Name" type="text" required />
+            <Input onChange={onChange} name="email" value={email} placeholder="Email" type="email" required />
+            <Input onChange={onChange} name="password" value={password} placeholder="Password" type="password" required />
+            
+            {/* submit 버튼을 클릭하면 Loading...이라는 글자를 보여주고 아니면 Create Account창을 계속 보여준다 */}
+            <Input type="submit" value={isLoading ? "Loading..." : "Create Account"} />
+        </Form>
+        {error !== "" ? <Error>{error}</Error> : null}
+    </Wrapper>
 }
