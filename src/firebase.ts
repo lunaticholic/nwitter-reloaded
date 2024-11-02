2c53eac75efcb94c9785c3a8a776147342e39994
lunaticholic <lunaticholic@gmail.com>
2024-11-02 08:18:38 +0100

    Nwitter Firebase settings

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBvfsX6925Lmi2XnZPW1W2X4k62ClNfiMc",
+  authDomain: "nwitter-reloaded-9659a.firebaseapp.com",
+  projectId: "nwitter-reloaded-9659a",
+  storageBucket: "nwitter-reloaded-9659a.firebasestorage.app",
+  messagingSenderId: "812486670431",
+  appId: "1:812486670431:web:bf8d73bc467e4b85fec3ea"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
